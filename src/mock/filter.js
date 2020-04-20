1d031a56096c5fed6c1bc50f2ef59c9cd8cb1c52
kirillpotekhin <kirill.potekhin@gmail.com>
2020-04-21 00:02:49 +0700

    Описал структуру фильтров, написал функции для вычисления актуального количества задач для каждого фильтра

---
+++ src/mock/filter.js
@@ -0,0 +1,50 @@
+const generateFilters = (tasks) => {
+  const date = new Date();
+  return [{
+    name: `all`,
+    count() {
+      return tasks.length;
+    },
+  }, {
+    name: `overdue`,
+    count() {
+      let summ = 0;
+      tasks.forEach(it => {
+        (it.dueDate !== null) ? (it.dueDate.getTime() < date) ? summ++ : summ : summ
+      });
+      return summ;
+    },
+  }, {
+    name: `today`,
+    count() {
+      let summ = 0;
+      tasks.forEach(it => {
+        (it.dueDate !== null) ? (it.dueDate.getFullYear() === date.getFullYear()) && (it.dueDate.getMonth() === date.getMonth()) && (it.dueDate.getDate() === date.getDate()) ? summ++ : summ : summ
+      });
+      return summ;
+    },
+  }, {
+    name: `favorites`,
+    count() {
+      let summ = 0;
+      tasks.forEach(it => (it.isFavorite) ? summ++ : summ)
+      return summ;
+    },
+  }, {
+    name: `repeating`,
+    count() {
+      let summ = 0;
+      tasks.forEach(it => Object.values(it.repeatingDays).some(Boolean) ? summ++ : summ)
+      return summ;
+    },
+  }, {
+    name: `archive`,
+    count() {
+      let summ = 0;
+      tasks.forEach(it => (it.isArchive) ? summ++ : summ)
+      return summ;
+    },
+  }];
+};
+
+export {generateFilters};
