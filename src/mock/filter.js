58f7cf306d029ffce7232d2b24d96ed811caeb76
kirillpotekhin <kirill.potekhin@gmail.com>
2020-04-21 16:25:41 +0700

    Применил фильтрацию для определения актуального количество подходящих карточек под фильтры

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -2,56 +2,22 @@ const generateFilters = (tasks) => {
   const date = new Date();
   return [{
     name: `all`,
-    count() {
-      return tasks.length;
-    },
+    count: () => tasks.length,
   }, {
     name: `overdue`,
-    count() {
-      let summ = 0;
-      tasks.forEach((it) => {
-        if (it.dueDate !== null) {
-          if (it.dueDate.getTime() < date) {
-            summ++;
-          }
-        }
-      });
-      return summ;
-    },
+    count: () => tasks.filter(it => it.dueDate && it.dueDate.getTime() < date).length,
   }, {
     name: `today`,
-    count() {
-      let summ = 0;
-      tasks.forEach((it) => {
-        if (it.dueDate !== null) {
-          if ((it.dueDate.getFullYear() === date.getFullYear()) && (it.dueDate.getMonth() === date.getMonth()) && (it.dueDate.getDate() === date.getDate())) {
-            summ++;
-          }
-        }
-      });
-      return summ;
-    },
+    count: () => tasks.filter(it => it.dueDate && (it.dueDate.toDateString() === date.toDateString())).length,
   }, {
     name: `favorites`,
-    count() {
-      let summ = 0;
-      tasks.forEach((it) => (it.isFavorite) ? summ++ : summ);
-      return summ;
-    },
+    count: () => tasks.filter(it => it.isFavorite).length,
   }, {
     name: `repeating`,
-    count() {
-      let summ = 0;
-      tasks.forEach((it) => Object.values(it.repeatingDays).some(Boolean) ? summ++ : summ);
-      return summ;
-    },
+    count: () => tasks.filter(it => Object.values(it.repeatingDays).some(Boolean)).length,
   }, {
     name: `archive`,
-    count() {
-      let summ = 0;
-      tasks.forEach((it) => (it.isArchive) ? summ++ : summ);
-      return summ;
-    },
+    count: () => tasks.filter(it => it.isArchive).length,
   }];
 };
 
