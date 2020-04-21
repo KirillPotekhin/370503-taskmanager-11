a83beec06ecce64d8a8e87e1875d2c5a19dfc616
kirillpotekhin <kirill.potekhin@gmail.com>
2020-04-21 16:50:49 +0700

    Доработал согласно ESLint

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -5,19 +5,19 @@ const generateFilters = (tasks) => {
     count: () => tasks.length,
   }, {
     name: `overdue`,
-    count: () => tasks.filter(it => it.dueDate && it.dueDate.getTime() < date).length,
+    count: () => tasks.filter((it) => it.dueDate && it.dueDate.getTime() < date).length,
   }, {
     name: `today`,
-    count: () => tasks.filter(it => it.dueDate && (it.dueDate.toDateString() === date.toDateString())).length,
+    count: () => tasks.filter((it) => it.dueDate && (it.dueDate.toDateString() === date.toDateString())).length,
   }, {
     name: `favorites`,
-    count: () => tasks.filter(it => it.isFavorite).length,
+    count: () => tasks.filter((it) => it.isFavorite).length,
   }, {
     name: `repeating`,
-    count: () => tasks.filter(it => Object.values(it.repeatingDays).some(Boolean)).length,
+    count: () => tasks.filter((it) => Object.values(it.repeatingDays).some(Boolean)).length,
   }, {
     name: `archive`,
-    count: () => tasks.filter(it => it.isArchive).length,
+    count: () => tasks.filter((it) => it.isArchive).length,
   }];
 };
 
