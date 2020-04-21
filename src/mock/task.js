fd7043b1bb62631332781cad684fc792e03eb2a2
kirillpotekhin <kirill.potekhin@gmail.com>
2020-04-21 16:44:33 +0700

    Изменил функцию для создания массива данных для карточек задач, без создания дополнительных задач

--- src/mock/task.js
+++ src/mock/task.js
@@ -24,7 +24,7 @@ const generateTask = () => {
 };
 
 const generateTasks = (count) => {
-  return new Array(count).fill(``).map(generateTask);
+  return Array.from({length: count}, generateTask);
 };
 
 export {generateTask, generateTasks};
